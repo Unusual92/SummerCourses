79a75b943b031b3e78c5d8086fd18f4832476298
Unusual92 <104244417+Unusual92@users.noreply.github.com>
2024-06-19 23:21:35 +0600

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+  <input type = "number" v-model="number"/>
+  <tableComponent :InputNumber="number"/>
+</template>
+
+<script>
+import tableComponent from "@/components/table";
+
+export default {
+  name: 'App',
+  components:{
+    tableComponent
+  }
+  ,
+  data(){
+    return{
+      number: 0,
+    }
+  }
+  }
+
+</script>
+
---
+++ table.vue
@@ -0,0 +1,67 @@
+<template lang="">
+    <div>
+        <button @click="IncrementValues">Увеличить значение</button>
+        <table>
+            <tr>
+                <th v-for="num in numbers" :key="num">
+                    {{num}}
+                </th>
+            </tr>
+            <tr>
+                <th v-for="num in numbers" :key="num">
+                    {{num * InputNumber}}
+                </th>
+            </tr>
+            <tr>
+                <th v-for="num in numbers" :key="num">
+                    {{(num * InputNumber)**2}}
+                </th>
+            </tr>
+            <tr>
+                <th v-for="num in numbers" :key="num">
+                    {{num**2}}
+                </th>
+            </tr>
+
+
+        </table>
+        
+    </div>
+</template>
+<script>
+
+
+export default {
+    name : "tableComponent",
+    data(){
+        return{
+            numbers:[1,2,3,4,5,6,7,8,9,10]
+        }
+    },
+    props:{
+        InputNumber: Number
+    }
+    ,
+    methods: {
+        IncrementValues(){
+           for(let i = 0; i < 10; i++){
+                this.numbers[i]++;
+           }
+            }
+        }
+    }
+    
+
+</script>
+<style scoped>
+    th{
+        width: 50px;
+        height: 50px;
+        border: 2px solid black;
+    }
+    td{
+        width: 50px;
+        height: 50px;
+        border: 2px solid black;
+    }
+</style>
